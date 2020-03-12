fb2aa0d651601e85bf30269706c73d16f45c988f
enzo <mollenthielenzo@gmail.com>
2020-03-12 17:07:14 +0100

    spring intialized

---
+++ src/main/java/com/tame/tameApi/TameApiApplication.java
@@ -0,0 +1,13 @@
+package com.tame.tameApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TameApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TameApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tame/tameApi/TameApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tame.tameApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TameApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
