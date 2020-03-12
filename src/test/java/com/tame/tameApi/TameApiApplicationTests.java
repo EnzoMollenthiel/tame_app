2cadd544d31de31e807ff8d4d0e7dd4ddd8d4922
enzo <mollenthielenzo@gmail.com>
2020-03-12 23:46:10 +0100

    implements test database

--- src/test/java/com/tame/tameApi/TameApiApplicationTests.java
+++ src/test/java/com/tame/tameApi/TameApiApplicationTests.java
@@ -1,9 +1,18 @@
 package com.tame.tameApi;
 
+import javafx.application.Application;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+import resources.H2TestProfileJPAConfig;
 
-@SpringBootTest
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = {
+		Application.class,
+		H2TestProfileJPAConfig.class})
+@ActiveProfiles("test")
 class TameApiApplicationTests {
 
 	@Test
