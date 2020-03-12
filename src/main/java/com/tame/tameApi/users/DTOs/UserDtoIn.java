6fd4fd0d9efd0beadfeba8919bade0259229017f
mathildefpsn <mathilde.person@ynov.com>
2020-03-12 16:23:18 +0100

    add user model, user dtos

---
+++ src/main/java/com/tame/tameApi/users/DTOs/UserDtoIn.java
@@ -0,0 +1,28 @@
+package com.tame.tameApi.users.DTOs;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserDtoIn {
+    private String email;
+    private String pseudo;
+    private String password;
+    private String phoneNumber;
+    private Date birthDate;
+    private Integer ageMax;
+    private Integer ageMin;
+    private Integer distance;
+    private Integer matchesNumber;
+    private Integer beenLikedNumber;
+    private Integer beenDislikedNumber;
+    private Integer didLikeNumber;
+    private Integer didDislikeNumber;
+    private String description;
+    private String city;
+}
