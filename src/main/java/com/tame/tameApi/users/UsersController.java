7b98ba3f957e007796cba1fac6e7d8033cbdad11
enzo <mollenthielenzo@gmail.com>
2020-03-13 01:51:35 +0100

    getById_should_return_a_user refactor

--- src/main/java/com/tame/tameApi/users/UsersController.java
+++ src/main/java/com/tame/tameApi/users/UsersController.java
@@ -1,25 +1,11 @@
 package com.tame.tameApi.users;
 
-import com.tame.tameApi.users.models.User;
-import com.tame.tameApi.users.repositories.UsersRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
+import com.tame.tameApi.users.DAOs.UsersDao;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @RestController
 public class UsersController {
 
-    private final UsersRepository usersRepository;
-
-    UsersController(@Autowired UsersRepository usersRepository){
-        this.usersRepository = usersRepository;
-    };
-
-    @GetMapping("/users")
-    List<User> getAll() {
-        return usersRepository.findAll();
-    }
+    private final UsersDao usersDao = new UsersDao();
 
 }
